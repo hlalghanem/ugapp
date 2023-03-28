aa8e34e3e7b1e9874ccf7004cc37a5625c5cef6d
hlalghanem <hlal@usaimigulf.com>
2023-03-28 10:54:00 +0300

    update sales blade

--- resources/views/sales/sales.blade.php
+++ resources/views/sales/sales.blade.php
@@ -1,38 +1,9 @@
 @extends('layouts.main-layout')
 @section('content')
 
-{{-- @if (is_null($branchIds))
-<p>No active branches found for this user.</p>
-@else
 
-<p> Branches#{{ $branchCount }}</p> 
-<p> IDs #{{ $branchIds }}</p>  --}}
 
-{{-- 
-
-<hr>
-<table>
-    <thead>
-        <tr>
-            <th>Customer Name</th>
-            <th>Total Amount Paid</th>
-            <th>Last End-of-Day Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($transactions as $transaction)
-            <tr>
-                <td>
-                    <a href="/sales/{{ $transaction->omega_id }}">{{ $transaction->cust_name }}</a>
-                    </td>
-                <td>{{ $transaction->{'SUM(amount_paid)'} }}</td>
-                <td>{{ $transaction->{'MAX(eod_date)'} }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table> --}}
-
-{{-- @endif --}}
+{{-- Updated --}}
 
 
 
